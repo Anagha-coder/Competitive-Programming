bc987f8878589c611d7f79a8d98a41479229b754
Anagha Desai <71918177+Anagha-coder@users.noreply.github.com>
2021-08-26 12:00:42 +0530

    Add files via upload

---
+++ PYTHON LEEETCODE/Longest Substring.py
@@ -0,0 +1,24 @@
+# leetcode python
+#Longest Substring Without Repeating Characters
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s)==0:
+            return 0
+        map ={} # created an empty dict
+        max_length = 0
+        start = 0
+        
+        for i in range(len(s)):
+            if s[i] in map and start >= map[s[i]]:
+                start= map[s[i]] + 1
+            else:
+                max_length = max(max_length, i- start + 1)
+                map[s[i]] = i
+        return max_length        
+        
